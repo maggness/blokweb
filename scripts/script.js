77916a5252adea150dbfa3266cbc09f6dc38d0fa
maggness <timderoller@gmail.com>
2020-09-10 13:24:21 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+
+var burger1 = document.querySelector('#burger1');
+var burger2 = document.querySelector('#burger2');
+var burger3 = document.querySelector('#burger3');
+var burgermenu = document.querySelector('#burgermenu');
+
+function burgerMenuPress() {
+  burger1.classList.toggle('burger1rot');
+  burger2.classList.toggle('burger2rot');
+  burger3.classList.toggle('burger3rot');
+}
+
+burgermenu.addEventListener("click", burgerMenuPress);

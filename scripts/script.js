25a5e851100f05c6dde43ff64eb693a623be0490
maggness <timderoller@gmail.com>
2020-09-15 21:58:16 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,13 @@ var burger1 = document.querySelector('#burger1');
 var burger2 = document.querySelector('#burger2');
 var burger3 = document.querySelector('#burger3');
 var burgermenu = document.querySelector('#burgermenu');
+var burgermenuklap = document.querySelector('#menu');
 
 function burgerMenuPress() {
   burger1.classList.toggle('burger1rot');
   burger2.classList.toggle('burger2rot');
   burger3.classList.toggle('burger3rot');
+  burgermenuklap.classList.toggle('klapmenuopen');
 }
 
 burgermenu.addEventListener("click", burgerMenuPress);
